3289b53cf632c21c9142cd075a08366b425b5259
Chocobouc <alexandre.poli.pro@gmail.com>
2024-11-27 05:37:59 +0100

    recover auth twitch

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import { AuthOptions, NextAuthOptions } from "next-auth";
+import TwitchProvider from "next-auth/providers/twitch";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    TwitchProvider({
+      clientId: process.env.TWITCH_ID as string,
+      clientSecret: process.env.TWITCH_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      return {
+        ...session,
+        access_token: token.access_token,
+        providerAccountId: token.providerAccountId,
+      };
+    },
+    async jwt({ token, user, account, profile }) {
+      return { ...token, ...account };
+    },
+  },
+} satisfies NextAuthOptions;
