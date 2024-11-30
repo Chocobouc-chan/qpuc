43bbe82d6dae632db9a1ee61987ae5c4abdee0bd
Chocobouc <alexandre.poli.pro@gmail.com>
2024-11-30 20:33:25 +0100

    build fix

--- app/qpuc/page.tsx
+++ app/qpuc/page.tsx
@@ -6,21 +6,25 @@ import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const session: (Session & { access_token: string }) | null =
-    await getServerSession(authOptions);
+  const session:
+    | (Session & { access_token: string; providerAccountId: string })
+    | null = await getServerSession(authOptions);
   let clips = [];
 
   //TODO remove and add proper error handling
   if (session) {
-    clips = await getTwitchClips(session);
-
-    return (
-      <div className="w-full h-full bg-gradient-to-r from-blue-500 to-fuchsia-700 overflow-y-hidden ">
-        <div className="flex flex-col items-center justify-center mt-4">
-          <Home session={session} clips={clips} />
+    clips = await getTwitchClips();
+    if (clips) {
+      return (
+        <div className="w-full h-full bg-gradient-to-r from-blue-500 to-fuchsia-700 overflow-y-hidden ">
+          <div className="flex flex-col items-center justify-center mt-4">
+            <Home session={session} clips={clips} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   } else {
     redirect("/");
   }
--- lib/twitchData.ts
+++ lib/twitchData.ts
@@ -1,8 +1,5 @@
 "use server";
-import { Session } from "next-auth";
-export async function getTwitchClips(
-  session: Session & { access_token: string }
-) {
+export async function getTwitchClips() {
   const res = await fetch(
     `${process.env.TWITCH_URL_API}/clips?broadcaster_id=${process.env.TWITCH_ID_BASHCLUB}&first=100`,
     {
